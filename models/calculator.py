843526104f5544ec330a819424a009f9dff7f829
Ianvrij <Ianvrij@hotmail.com>
2024-12-03 17:39:36 +0100

    Add TCO calculation logic

--- models/calculator.py
+++ models/calculator.py
@@ -1,1 +1,5 @@
+def calculate_tco(distance, load_capacity, fuel_price, maintenance_cost, insurance_cost):
+    # Example calculation for Total Cost of Ownership (TCO)
+    tco = (distance * fuel_price) + maintenance_cost + insurance_cost
+    return tco
 
